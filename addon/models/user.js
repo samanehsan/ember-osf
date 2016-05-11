a940c1ead90aa3eebfab56251c5b210afb8e437c
Saman Ehsan <se8ea@virginia.edu>
2016-05-11 13:56:14 -0400

    Add more fields to user model

--- addon/models/user.js
+++ addon/models/user.js
@@ -3,9 +3,15 @@ import DS from 'ember-data';
 import OsfModel from 'ember-osf/models/base';
 
 export default OsfModel.extend({
+    full_name: DS.attr('string'),
     given_name: DS.attr('string'),
     middle_names: DS.attr(),
     family_name: DS.attr('string'),
+    suffix: DS.attr('string'),
 
-    nodes: DS.hasMany('nodes')
+    date_registered: DS.attr('date'),
+
+    nodes: DS.hasMany('nodes'),
+    registrations: DS.hasMany('registrations'),
+    institutions: DS.hasMany('institutions')
 });
